e2276ae1656065ab12447f41c02a504e84ae794f
cmobley001 <cmobley001@student.butte.edu>
2024-07-13 06:52:16 -0700

    updates to JS

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 'use strict';
 
-console.log(document.querySelector('.message'));
+// document.querySelector('string').textContent = 'string';
+// document.querySelector('string').addEventListener('click' function() {});
+
+// document.querySelector('.message').textContent = 'Correct Number!';
+// document.querySelector('.number').textContent = '13';
+// document.querySelector('.score').textContent = '10';
+// document.querySelector('.guess').value = '23';
+
+// game logic
+const randomNumber = Math.random() * 10;
+const truncatedNumber = Math.trunc(randomNumber);
+console.log(truncatedNumber);
+
+
+
+
+// the addEventListener() method attaches an event handler to the specified element
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = '🚫 Enter a Number!';
+    } else {
+        document.querySelector('.message').textContent = '👍 Correct Number!';
+    }
+}) 
