98d697c0db808d65ec9fecb43ae876dd4f6ef960
cmobley001 <cmobley001@student.butte.edu>
2024-07-16 05:24:54 -0700

    #76 changes

--- script.js
+++ script.js
@@ -23,28 +23,48 @@ document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     document.querySelector('.number').textContent = guess;
 
+    //reset game
+    //*document.querySelector('.again').addEventListener('click', location.reload());*
+
+
     // if (guess > 20) {
     //     document.querySelector('.message').textContent = '🚫 Enter a Number between 1 and 20!';
     // }
     if (!guess) {
+
+        //no input error message
         document.querySelector('.message').textContent = '🚫 Enter a Number!';
+
+        //correct number
     } else if (guess === guessNumber) {
         document.querySelector('.message').textContent = '👍 Correct Number!';
+        document.querySelector('body').style.backgroundColor = '#6987bb';
+        document.querySelector('.number').style.width = '23rem';
+
+
     } else if (guess > guessNumber) {
         if (score > 1) {
+       // guess too high
             document.querySelector('.message').textContent = '📈 Too High!';
             score--;
             document.querySelector('.score').textContent = score;
         } else {
+        //user lost game
             document.querySelector('.message').textContent = '👎 You Lost!';
             document.querySelector('.score').textContent = 0;
         }
     } else if (guess < guessNumber) {
         if (score > 1) {
+
+        // guess too low
         document.querySelector('.message').textContent = '📉 Too Low!';
+
+        //attempts counter
         score--;
         document.querySelector('.score').textContent = score;
         } else {
+
+        //user lost game
             document.querySelector('.message').textContent = '👎 You Lost!';
             document.querySelector('.score').textContent = 0;
         }
